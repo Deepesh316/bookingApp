16dc05c5ae440dc80aab4b1148ff19fcbc95fd90
Deepesh Kumar R <deepeshkumarr316@gmail.com>
2020-01-23 12:57:04 +0530

    Add: Header component & SCSS styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'booking-app';
+  AppTitle = "Booking Application";
+  clickHandler() {
+    alert("Clicked Me!!");
+  }
 }
